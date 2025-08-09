75767c7e0b19af6f99a9595f2e4d95454985257e
Sven Haardiek <sven.haardiek@uni-muenster.de>
2025-08-09 13:45:09 +0200

    Introduce User Management
    This commit introduces user management in a very rudimentary form:
    
    * Users can login and out. The status is stored in a session cookie
    * There are currently no user endpoint to manage users from the UI, but
      at the startup a `admin` user is either created or updated with a
      password provided via environment variable.
    
    Closes #7

--- frontend/src/Pages/Home.vue
+++ frontend/src/Pages/Home.vue
@@ -3,9 +3,13 @@ import DefaultLayout from "@/Layouts/DefaultLayout.vue";
 import VerticalMenu from "@/Components/VerticalMenu.vue";
 import LinkButton from "@/Components/LinkButton.vue";
 import Button from "@/Components/Button.vue";
-import { apiCreateList } from "@/api/api.ts";
+import { apiCreateList, apiDeleteSession, apiGetSession } from "@/api/api.ts";
 import { router } from "@/router.ts";
 import { type List } from "@/types.ts";
+import { onMounted, ref } from "vue";
+
+const loggedIn = ref<boolean>(false);
+const loaded = ref<boolean>(false);
 
 async function listAsGuest() {
   try {
@@ -19,6 +23,25 @@ async function listAsGuest() {
     alert("Unable to get list from server: " + error);
   }
 }
+
+async function checkSession() {
+  try {
+    await apiGetSession();
+    loggedIn.value = true;
+  } catch (error) {
+    loggedIn.value = false;
+  }
+}
+
+async function logout() {
+  await apiDeleteSession();
+  loggedIn.value = false;
+}
+
+onMounted(async () => {
+  await checkSession();
+  loaded.value = true;
+});
 </script>
 
 <template>
@@ -27,8 +50,10 @@ async function listAsGuest() {
       <h1>Listinator</h1>
     </template>
     <template v-slot:main>
-      <VerticalMenu>
+      <VerticalMenu v-if="loaded">
         <Button @click="listAsGuest">New List as Guest</Button>
+        <LinkButton v-if="!loggedIn" to="/login">Login</LinkButton>
+        <Button v-if="loggedIn" @click="logout">Logout</Button>
       </VerticalMenu>
     </template>
   </DefaultLayout>
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -2,10 +2,12 @@ import { createWebHashHistory, createRouter } from "vue-router";
 
 import Home from "./Pages/Home.vue";
 import List from "./Pages/List.vue";
+import Login from "./Pages/Login.vue";
 
 const routes = [
   { path: "/", component: Home, name: "home" },
   { path: "/list/:id", component: List, name: "list" },
+  { path: "/login", component: Login, name: "login" },
 ];
 
 export const router = createRouter({
