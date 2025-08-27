4de34b00d2d253c89c4675ea6b3a813578286fa6
Sven Haardiek <sven.haardiek@uni-muenster.de>
2025-08-27 20:57:20 +0200

    SSE instead of interval Updates
    This commit introduces Server Side Events to send updates directly to
    clients. This way changed entries are shown immediately.
    
    Closes #27

--- api/v1/server/entry.go
+++ api/v1/server/entry.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
@@ -58,6 +60,10 @@ func (s server) entryCreate() echo.HandlerFunc {
 			return echo.ErrInternalServerError.SetInternal(err)
 		}
 
+		s.entryPubSub.Publish(e.ListID, entryEvent{
+			Action: "create",
+			Entry:  e,
+		})
 		return c.JSON(http.StatusCreated, e)
 	}
 }
@@ -77,13 +83,8 @@ func (s server) entryUpdate() echo.HandlerFunc {
 			return echo.ErrBadRequest.SetInternal(err)
 		}
 
-		e := database.Entry{
-			Model: database.Model{
-				ID: i.ID,
-			},
-		}
-		if err := s.db.First(&e).Error; err != nil {
-			// TODO: handle different errors
+		var e database.Entry
+		if err := s.db.First(&e, i.ID).Error; err != nil {
 			return echo.NotFoundHandler(c)
 		}
 		e.Name = i.Name
@@ -96,6 +97,10 @@ func (s server) entryUpdate() echo.HandlerFunc {
 			return echo.ErrInternalServerError.SetInternal(err)
 		}
 
+		s.entryPubSub.Publish(e.ListID, entryEvent{
+			Action: "update",
+			Entry:  e,
+		})
 		return c.JSON(http.StatusOK, e)
 	}
 }
@@ -110,15 +115,98 @@ func (s server) entryDelete() echo.HandlerFunc {
 			return echo.ErrBadRequest.SetInternal(err)
 		}
 
-		e := database.Entry{
-			Model: database.Model{
-				ID: i.ID,
-			},
-		}
-		if err := s.db.Delete(&e).Error; err != nil {
-			// TODO: handle different errors
+		var e database.Entry
+		if err := s.db.First(&e, i.ID).Error; err != nil {
 			return echo.NotFoundHandler(c)
 		}
+		if err := s.db.Delete(&e).Error; err != nil {
+			return echo.ErrInternalServerError.SetInternal(fmt.Errorf("unable to delete entry %v, %w", e, err))
+		}
+		s.entryPubSub.Publish(e.ListID, entryEvent{
+			Action: "delete",
+			Entry:  e,
+		})
 		return c.JSON(http.StatusOK, e)
+	}
+}
+
+type entryEvent struct {
+	Action string
+	Entry  database.Entry
+}
+
+func (s server) entryGetEvents() echo.HandlerFunc {
+	type input struct {
+		ListID uuid.UUID `query:"ListID"`
+	}
+
+	// sendEvent is a helper function to simplify sending SSE Events
+	sendEvent := func(c echo.Context, event string, data string) error {
+		w := c.Response()
+		defer w.Flush()
+		if _, err := fmt.Fprintf(w, "id: %v\n", time.Now().UnixMilli()); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprintf(w, "data: %v\n", data); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprintf(w, "event: %v\n", event); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprintf(w, "\n"); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// ping just send a ping SSE Event with the current time
+	ping := func(c echo.Context) error {
+		if err := sendEvent(c, "ping", "ping"); err != nil {
+			return fmt.Errorf("failed to send event, %w", err)
+		}
+		return nil
+	}
+
+	return func(c echo.Context) error {
+		var i input
+		if err := c.Bind(&i); err != nil {
+			return echo.ErrBadRequest.SetInternal(err)
+		}
+
+		// subscribe to the pubsub channel for this list
+		id, ch, err := s.entryPubSub.Subscribe(i.ListID)
+		if err != nil {
+			return echo.ErrInternalServerError.SetInternal(fmt.Errorf("unable to subscribe, %w", err))
+		}
+		// unsubscribe after return
+		defer s.entryPubSub.Unsubscribe(i.ListID, id)
+
+		w := c.Response()
+		// set header for SSE
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		for {
+			select {
+			// return, if connection is closed from client
+			case <-c.Request().Context().Done():
+				return nil
+			// send a ping every 5 seconds to keep the SSE connection. This can probably be less than that
+			case <-time.After(5 * time.Second):
+				if err := ping(c); err != nil {
+					return echo.ErrInternalServerError.SetInternal(fmt.Errorf("failed to ping, %w", err))
+				}
+			// receive data from pubsub channel and send them to the client
+			case ee := <-ch:
+				entry, err := json.Marshal(ee.Entry)
+				if err != nil {
+					return echo.ErrInternalServerError.SetInternal(fmt.Errorf("failed to marshal JSON, %w", err))
+				}
+				if err := sendEvent(c, ee.Action, string(entry)); err != nil {
+					return echo.ErrInternalServerError.SetInternal(fmt.Errorf("unable to send event, %w", err))
+				}
+			}
+		}
 	}
 }
---
+++ pubsub/pubsub.go
@@ -0,0 +1,94 @@
+// Package pubsub
+package pubsub
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type PubSub[K comparable, T any] struct {
+	buffer int
+	logger echo.Logger
+	m      *sync.Mutex
+	store  map[K]map[uuid.UUID]chan T
+}
+
+func New[K comparable, T any](logger echo.Logger, buffer int) PubSub[K, T] {
+	return PubSub[K, T]{
+		m:      &sync.Mutex{},
+		store:  map[K]map[uuid.UUID]chan T{},
+		logger: logger,
+		buffer: buffer,
+	}
+}
+
+// getOrInit gets or initializes the store part for the Key k.
+// This is for internal use and has therefor the Mutex not locked, so use with care.
+func (ps PubSub[K, T]) getOrInit(k K) map[uuid.UUID]chan T {
+	subs, ok := ps.store[k]
+
+	// Key is not present, so init it.
+	if !ok {
+		ps.store[k] = map[uuid.UUID]chan T{}
+		subs = ps.store[k]
+	}
+
+	return subs
+}
+
+func (ps PubSub[K, T]) Publish(k K, t T) error {
+	ps.m.Lock()
+	defer ps.m.Unlock()
+
+	subs := ps.getOrInit(k)
+
+	// Send to all subs parallel and with timeout
+	var wg sync.WaitGroup
+	wg.Add(len(subs))
+	for k, sub := range subs {
+		go func(k uuid.UUID, ch chan T) {
+			select {
+			case ch <- t:
+			case <-time.After(100 * time.Millisecond):
+				ps.logger.Errorf("failed to send to sub %v", k)
+			}
+			wg.Done()
+		}(k, sub)
+	}
+	wg.Wait()
+	return nil
+}
+
+func (ps PubSub[K, T]) Subscribe(k K) (uuid.UUID, chan T, error) {
+	ps.m.Lock()
+	defer ps.m.Unlock()
+
+	subs := ps.getOrInit(k)
+
+	// add new sub to subs
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return id, nil, fmt.Errorf("unable to create uuid, %w", err)
+	}
+	subs[id] = make(chan T, ps.buffer)
+
+	return id, subs[id], nil
+}
+
+func (ps PubSub[K, T]) Unsubscribe(k K, id uuid.UUID) {
+	ps.m.Lock()
+	defer ps.m.Unlock()
+
+	subs, ok := ps.store[k]
+
+	// Key is not present, so there is nothing to unsubscribe
+	if !ok {
+		return
+	}
+	close(subs[id])
+	delete(subs, id)
+}
